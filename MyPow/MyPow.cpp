461029cef4bb1c9244cf7f36dad0601fcff4e6bc
Muaaz Butt <132152284+Muaaz-Butt@users.noreply.github.com>
2023-08-26 02:50:56 -0700

    MyPow problem of leetcode

---
+++ MyPow/MyPow.cpp
@@ -0,0 +1,19 @@
+double myPow(double x, int n) {
+    if (n == 0) {
+        return 1.0;
+    }
+
+    double half = myPow(x, n / 2);
+
+    if (n % 2 == 0) {
+        return half * half;
+    }
+    else {
+        if (n > 0) {
+            return x * half * half;
+        }
+        else {
+            return (1.0 / x) * half * half;
+        }
+    }
+}

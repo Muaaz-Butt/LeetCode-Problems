b7491512f469b68e598f3ba143cbdc5c39d53702
Muaaz Butt <132152284+Muaaz-Butt@users.noreply.github.com>
2023-09-20 11:14:39 +0500

    leet code problem Reverse Linked List

---
+++ ReverseLinkedList/ReverseLinkedList.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* current = head;
+        ListNode* nextTemp;
+        while (current != nullptr) {
+            nextTemp = current->next;
+            current->next = prev;
+            prev = current;
+            current = nextTemp;
+        }
+        return prev;
+    }
+};

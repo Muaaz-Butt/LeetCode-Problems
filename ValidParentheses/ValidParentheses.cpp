1cec9219c4bc7b0093440060615a1be9fcb81270
Muaaz Butt <132152284+Muaaz-Butt@users.noreply.github.com>
2023-09-29 16:42:45 +0500

    Leetcode problem Valid Parenthesis
    Beats 100% user in time complexity

---
+++ ValidParentheses/ValidParentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            }
+            else {
+                if (st.empty() || (s[i] == ')' && st.top() != '(') || (s[i] == '}' && st.top() != '{') || (s[i] == ']' && st.top() != '[')) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};

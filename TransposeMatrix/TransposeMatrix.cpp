48d092a087980be4298f0f3b9090deab89402916
Muaaz Butt <132152284+Muaaz-Butt@users.noreply.github.com>
2023-11-08 12:53:32 +0500

    Transpose Matrix leetcode problem

---
+++ TransposeMatrix/TransposeMatrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<vector<int>> result(cols, vector<int>(rows, 0));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+};

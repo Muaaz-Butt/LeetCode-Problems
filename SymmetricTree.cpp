b984ca19e10d00f48a14a223faea6abb4ee69bc5
Muaaz Butt <132152284+Muaaz-Butt@users.noreply.github.com>
2024-01-12 17:03:36 +0500

    Symmetric Tree leetcode problem
    make helper function pass root->left child and root->right child and check their mirror values

---
+++ SymmetricTree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isMirror(TreeNode* left, TreeNode* right) {
+        if (left == nullptr && right == nullptr) {
+            return true;
+        }
+        if (left == nullptr || right == nullptr)
+        {
+            return false;
+        }
+        return (left->val == right->val) && isMirror(left->left, right->right) && isMirror(left->right, right->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr) {
+            return true;
+        }
+        return isMirror(root->left, root->right);
+    }
+};

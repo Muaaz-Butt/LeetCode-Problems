573c0ad468e6b07237b5e338e8d466dbd0aa98ba
Muaaz Butt <132152284+Muaaz-Butt@users.noreply.github.com>
2024-07-01 17:53:07 +0500

    Time: 1 ms (67.01%), Space: 2.1 MB (18.56%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn find_peak_element(nums: Vec<i32>) -> i32 {
+        let mut left = 0;
+        let mut right = nums.len() - 1;
+        while left < right {
+            let mut mid = left + (right - left) / 2;
+            if nums[mid] < nums[mid + 1] {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        left as i32
+    }
+}

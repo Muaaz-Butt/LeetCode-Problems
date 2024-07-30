5f1dc18903f65364e2e40ad7f2da7c945999e545
Muaaz Butt <132152284+Muaaz-Butt@users.noreply.github.com>
2024-07-30 10:39:43 +0500

    Time: 40 ms (69.99%), Space: 16.7 MB (78.09%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_to_t = {}
+        t_to_s = {}
+
+        for char_s, char_t in zip(s, t):
+            if char_s in s_to_t:
+                if s_to_t[char_s] != char_t:
+                    return False
+            else:
+                s_to_t[char_s] = char_t
+            
+            if char_t in t_to_s:
+                if t_to_s[char_t] != char_s:
+                    return False
+            else:
+                t_to_s[char_t] = char_s
+        return True

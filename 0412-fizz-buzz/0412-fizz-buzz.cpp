abca966c474f087ff192a4ba65795d257db18365
Muaaz Butt <132152284+Muaaz-Butt@users.noreply.github.com>
2024-08-24 15:02:01 +0500

    Time: 0 ms (100.00%), Space: 11.6 MB (23.21%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        std::vector<std::string> result;
+
+        for (int i = 1; i <= n; ++i) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.push_back("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.push_back("Fizz");
+            } else if (i % 5 == 0) {
+                result.push_back("Buzz");
+            } else {
+                result.push_back(std::to_string(i));
+            }
+        }
+        return result;
+    }
+};

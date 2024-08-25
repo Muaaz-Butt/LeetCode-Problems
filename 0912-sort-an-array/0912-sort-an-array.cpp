73b7f52296fb4fac70c3369e4c81c29676c85708
Muaaz Butt <132152284+Muaaz-Butt@users.noreply.github.com>
2024-08-25 21:11:01 +0500

    Time: 395 ms (31.97%), Space: 178.4 MB (22.14%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    void merge(std::vector<int>& nums, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        std::vector<int> L(n1), R(n2);
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = nums[left + i];
+        for (int i = 0; i < n2; ++i)
+            R[i] = nums[mid + 1 + i];
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                nums[k] = L[i];
+                i++;
+            } else {
+                nums[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            nums[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            nums[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void mergeSort(std::vector<int>& nums, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            mergeSort(nums, left, mid);
+            mergeSort(nums, mid + 1, right);
+
+            merge(nums, left, mid, right);
+        }
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+};

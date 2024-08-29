10614ac4f78cc20e7d5116025e4120380ae8d514
Muaaz Butt <132152284+Muaaz-Butt@users.noreply.github.com>
2024-08-29 18:37:36 +0500

    Time: 1429 ms (13.80%), Space: 0B (100.00%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,15 @@
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM
+    Employee e
+    JOIN Department d ON e.departmentId = d.id
+WHERE
+    (
+        SELECT COUNT(DISTINCT salary)
+        FROM Employee e2
+        WHERE e2.departmentId = e.departmentId AND e2.salary >= e.salary
+    ) <= 3
+ORDER BY
+    Department, Salary DESC;
